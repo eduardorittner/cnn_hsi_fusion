68555a55811746f3d5719109f4e692122421307d
eduardorittner <eduardorittnerc@gmail.com>
2024-10-11 22:59:27 -0300

    Refactor of utils/loss
    Moved some things from loss to log.py

--- utils/loss.py
+++ utils/loss.py
@@ -1,70 +1,6 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import os
-import logging
-
-
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def initialize_logger(file_dir):
-    logger = logging.getLogger()
-    fhandler = logging.FileHandler(filename=file_dir, mode="a")
-    formatter = logging.Formatter("%(asctime)s - %(message)s", "%Y-%m-%d %H:%M:%S")
-    fhandler.setFormatter(formatter)
-    logger.addHandler(fhandler)
-    logger.setLevel(logging.INFO)
-    return logger
-
-
-def save_checkpoint(model_path, epoch, iteration, model, optimizer):
-    state = {
-        "epoch": epoch,
-        "iter": iteration,
-        "state_dict": model.state_dict(),
-        "optimizer": optimizer.state_dict(),
-    }
-
-    torch.save(state, os.path.join(model_path, "net_%depoch.pth" % epoch))
-
-
-def time2file_name(time):
-    year = time[0:4]
-    month = time[5:7]
-    day = time[8:10]
-    hour = time[11:13]
-    minute = time[14:16]
-    second = time[17:19]
-    time_filename = (
-        year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second
-    )
-    return time_filename
-
-
-def record_loss(loss_csv, epoch, iteration, epoch_time, lr, train_loss, test_loss):
-    """Record many results."""
-    loss_csv.write(
-        "{},{},{},{},{},{}\n".format(
-            epoch, iteration, epoch_time, lr, train_loss, test_loss
-        )
-    )
-    loss_csv.flush()
-    loss_csv.close
 
 
 class Loss_MRAE(nn.Module):
@@ -74,7 +10,7 @@ class Loss_MRAE(nn.Module):
     def forward(self, output, target):
         assert output.shape == target.shape
         error = torch.abs(output - target) / target
-        mrae = torch.mean(error.view(-1))
+        mrae = torch.mean(error.flatten())
         return mrae
 
 
@@ -86,7 +22,7 @@ class Loss_RMSE(nn.Module):
         assert output.shape == target.shape
         error = output - target
         sqrt_error = torch.pow(error, 2)
-        rmse = torch.sqrt(torch.mean(sqrt_error.view(-1)))
+        rmse = torch.sqrt(torch.mean(sqrt_error.flatten()))
         return rmse
 
 

08521d109b54f3363b581a43b4f09068a652c616
eduardorittner <eduardorittnerc@gmail.com>
2024-10-22 18:58:35 -0300

    lower number of epochs
    since we've increased the number of iterations per epoch we need less
    epochs

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -1,8 +1,8 @@
-python ../train.py --model mst --end-epoch 250 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+python ../train.py --model mst --end-epoch 200 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
 wait
-python ../train.py --model mst_plus_plus --end-epoch 250 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+python ../train.py --model mst_plus_plus --end-epoch 200 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
 wait
-python ../train.py --model restormer --end-epoch 250 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+python ../train.py --model restormer --end-epoch 200 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
 wait
-python ../train.py --model hdnet --end-epoch 250 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+python ../train.py --model hdnet --end-epoch 200 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
 wait

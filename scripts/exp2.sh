ac2ab85372bb3034c473370c5030d6fc598501f0
eduardorittner <eduardorittnerc@gmail.com>
2024-10-28 20:21:47 -0300

    exp2.sh
    Retraining restormer with 10 images per batches, which is the first
    setting that doesn't run out of memory. The other models run fine at 20
    images per batch.
    Also decreased the max number of epochs since due to early stopping they
    were all stopping very early anyway, the three models from exp1.sh stoppe
    d before 60 epochs.

---
+++ scripts/exp2.sh
@@ -0,0 +1,8 @@
+python ../train.py --model restormer --end-epoch 150 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64 --batch 10
+wait
+python ../train.py --model mprnet --end-epoch 150 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+wait
+python ../train.py --model mirnet --end-epoch 150 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+wait
+python ../train.py --model hinet --end-epoch 150 --outf ../exp/ --data-root ../data/ --patch-size 128 --stride 64
+wait
